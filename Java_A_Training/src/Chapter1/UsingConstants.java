641d03ef0064657ab497c0a1da58c96bdc3fbd75
Tumelo Maphalla <Tumelo.Maphalla@ilabqa.com>
2025-01-06 11:27:19 +0200

    Program created using different data types #1

---
+++ Java_A_Training/src/Chapter1/UsingConstants.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+public class UsingConstants {
+    public static void main(String[] args) {
+        int age;
+        int birthYear;
+        final int CURRENT_YEAR = 2025;
+        final String COMPANY = "iLab Quality";
+    }
+}
