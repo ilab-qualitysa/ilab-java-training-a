36123d1ec162235190e13156724374645c86035e
Tumelo Maphalla <Tumelo.Maphalla@ilabqa.com>
2025-01-06 12:17:20 +0200

    Learning objectives for chapter 1 complteted close #1

--- Java_A_Training/src/Chapter1/UserInput.java
+++ Java_A_Training/src/Chapter1/UserInput.java
@@ -1,5 +1,7 @@
 package Chapter1;
 
+import java.util.Scanner;
+
 public class UserInput
 {
     public static void main(String[] args) {
@@ -12,5 +14,28 @@ public class UserInput
         final String COMPANY_NAME = "iLab Quality";
         final double HOURLY_RATE = 350;
         final double TAX = 0.25;
+
+        Scanner sc = new Scanner(System.in);
+
+        //Assign
+        System.out.println("Please enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Enter birth year: ");
+        birthYear = sc.nextInt();
+        System.out.println("Enter hours worked: ");
+        hoursWorked = sc.nextInt();
+        System.out.println("Please specify position code: (S - senior, J -Junior, I - Intern)T");
+        positionCode = sc.next().charAt(0);
+
+        age = CURRENT_YEAR - birthYear;
+        salary = (hoursWorked * HOURLY_RATE) - (hoursWorked * HOURLY_RATE * TAX);
+
+        //USE
+        System.out.println("=================" + COMPANY_NAME + "=========================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hours worked: " + hoursWorked);
+        System.out.println("Position code: " + positionCode);
+        System.out.println("Salary: " + salary);
     }
 }
