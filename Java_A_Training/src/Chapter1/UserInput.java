613c999901a69033d7c830d4e7dee01ebe3b012f
Tumelo Maphalla <Tumelo.Maphalla@ilabqa.com>
2025-01-06 11:55:41 +0200

    Program created using different data types #1

---
+++ Java_A_Training/src/Chapter1/UserInput.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+public class UserInput
+{
+    public static void main(String[] args) {
+        //DECLARE
+        int age, birthYear, hoursWorked;
+        double salary;
+        String name;
+        char positionCode;
+        final int CURRENT_YEAR = 2025;
+        final String COMPANY_NAME = "iLab Quality";
+        final double HOURLY_RATE = 350;
+        final double TAX = 0.25;
+    }
+}
--- Java_A_Training/src/Chapter1/UsingConstants.java
+++ Java_A_Training/src/Chapter1/UsingConstants.java
@@ -4,7 +4,27 @@ public class UsingConstants {
     public static void main(String[] args) {
         int age;
         int birthYear;
+        double salary;
+        int hoursWorked;
+        String name;
         final int CURRENT_YEAR = 2025;
-        final String COMPANY = "iLab Quality";
+        final String COMPANY_NAME = "iLab Quality";
+        final double HOURLY_RATE = 350;
+        final double TAX = 0.25;
+
+        //Assign
+        name = "Tumelo";
+        birthYear = 2002;
+        age = CURRENT_YEAR - birthYear;
+        hoursWorked = 200;
+        salary = (hoursWorked * HOURLY_RATE) - (hoursWorked * HOURLY_RATE * TAX);
+
+        //USE
+        System.out.println("=================" + COMPANY_NAME + "=========================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hours worked: " + hoursWorked);
+        System.out.println("Salary: " + salary);
+
     }
 }
