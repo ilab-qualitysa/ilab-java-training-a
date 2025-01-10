acfcb035fe46929dad06d0ea16ddca89897aae08
Tumelo Maphalla <Tumelo.Maphalla@ilabqa.com>
2025-01-10 13:50:40 +0200

    Added package name Chapter6 and changed the loop varible from i-- to i++ to prevent an infinite loop

---
+++ Java_A_Training/src/Chapter6/DebugExercise8.java
@@ -0,0 +1,9 @@
+package Chapter6;
+
+public class DebugExercise8 {
+  public static void main(String[] args) {         
+   for (int i = 0; i < 5; i++) {
+     System.out.println("Index: " + i);         
+   }     
+  } 
+}
