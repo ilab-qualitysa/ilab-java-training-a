32b77621c9e797d994eb5d1b4467ad46c9883440
Tumelo Maphalla <Tumelo.Maphalla@ilabqa.com>
2025-01-10 13:39:14 +0200

    Added package name Chapter6 and changed the variables type from int to double for correct output of division

---
+++ Java_A_Training/src/Chapter6/DebugExercise4.java
@@ -0,0 +1,10 @@
+package Chapter6;
+
+public class DebugExercise4 {
+   public static void main(String[] args) {         
+  double x = 5;
+  double y = 2;
+  double result = x/y;
+  System.out.println("Result: " + result);     
+  } 
+}
